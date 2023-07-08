65c9696ca3e27add8c10aa73bc2a25c5b4344070
Rafael Leonan <faelleonan@gmail.com>
2023-07-08 12:20:32 -0300

    Add suporte a Coroutines ao Room

--- app/src/main/java/br/com/alura/orgs/database/dao/ProdutoDao.kt
+++ app/src/main/java/br/com/alura/orgs/database/dao/ProdutoDao.kt
@@ -12,19 +12,19 @@ import br.com.alura.orgs.model.Produto
 interface ProdutoDao {
 
     @Query("SELECT * FROM Produto")
-    fun buscaTodos(): List<Produto>
+    suspend fun buscaTodos(): List<Produto>
 
     @Insert(onConflict = OnConflictStrategy.REPLACE)
-    fun salva(vararg produto: Produto)
+    suspend fun salva(vararg produto: Produto)
 
     @Delete
-    fun remove(produto: Produto)
+    suspend fun remove(produto: Produto)
 
     @Update
-    fun altera(produto: Produto)
+    suspend fun altera(produto: Produto)
 
     @Query("SELECT * FROM Produto WHERE id = :id")
-    fun buscaPorId(id: Long): Produto?
+    suspend fun buscaPorId(id: Long): Produto?
     @Query("SELECT * FROM Produto ORDER BY "+
             "CASE :column "+
             "WHEN 'nome' THEN nome "+
@@ -32,7 +32,7 @@ interface ProdutoDao {
             "WHEN 'valor' THEN valor "+
             "ELSE id "+
             "END ASC")
-    fun listaOrdernadaAsc(column: String?): List<Produto>
+    suspend fun listaOrdernadaAsc(column: String?): List<Produto>
 
     @Query("SELECT * FROM Produto ORDER BY "+
             "CASE :column "+
@@ -41,5 +41,5 @@ interface ProdutoDao {
             "WHEN 'valor' THEN valor "+
             "ELSE id "+
             "END DESC")
-    fun listaOrdernadaDesc(column: String?): List<Produto>
+    suspend fun listaOrdernadaDesc(column: String?): List<Produto>
 }
